2a5e93a531e1e9450db1132844168780f0a141c8
EPICZEUS <s77broz@gmail.com>
2019-01-18 14:11:05 -0500

    Add minor improvements

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -1,6 +1,6 @@
 class Encounter < ApplicationRecord
-  has_many :player_encounters
-  has_many :players, through: :player_encounters
-  has_many :creature_encounters
-  has_many :creatures, through: :creature_encounters
+  # has_many :player_encounters
+  has_and_belongs_to_many :players
+  # has_many :creature_encounters
+  has_and_belongs_to_many :creatures
 end
